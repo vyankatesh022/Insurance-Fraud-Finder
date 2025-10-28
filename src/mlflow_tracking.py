53e4a6353bcb8e03fa49815582da7bb4e2006c8c
v <vyankatesh02@hotmail.com>
2025-10-28 18:05:38 +0530

    change mlflow tracking and add requirement.txt

--- src/mlflow_tracking.py
+++ src/mlflow_tracking.py
@@ -22,24 +22,13 @@ if not logger.handlers:
     logger.addHandler(file_handler)
 
 def setup_mlflow():
-    if os.getenv('GITHUB_ACTIONS')=='true':
-        logger.info('Running github → using DagsHub MLflow tracking.')
-        dagshub_name = os.getenv('DAGSHUB_NAME', '')
-        dagshub_repro = os.getenv('DAGSHUB_REPRO', '')
-
-        if not dagshub_name or not dagshub_repro:
-            logger.warning("⚠️ DAGSHUB_NAME or DAGSHUB_REPRO environment variables are not set.")
-
-        # Dagshub setup
-        dagshub.init(repo_owner=dagshub_name, \
-                    repo_name=dagshub_repro, mlflow=True)
-
-        uri=f'https://dagshub.com/{dagshub_name}/{dagshub_repro}.mlflow'
-        os.environ['MLFLOW_USERNAME']=dagshub_name
-        os.environ['MLFLOW_SECRET_KEY']=os.getenv("DAGSHUB_KEY", "")
-    else:
-        logger.info("Running locally → using local MLflow tracking.")
-        uri = "http://127.0.0.1:5000"
+    dagshub_name='vyankatesh'
+    dagshub_repro='Insurance-Fraud-Finder'
+    
+    # Dagshub setup
+    dagshub.init(repo_owner=dagshub_name,repo_name=dagshub_repro, mlflow=True)
+    
+    uri=f'https://dagshub.com/{dagshub_name}/{dagshub_repro}.mlflow'
 
     mlflow.set_tracking_uri(uri)
     logger.info(f"MLflow tracking URI set to: {uri}")
